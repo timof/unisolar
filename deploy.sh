612f18c1464e469e5d6ebcffb86093e5b2bf8130
Timo Felbinger <gittf@qipc.org>
2011-06-22 08:01:44 +0200

    exclude deploy.sh.new

--- deploy.sh
+++ deploy.sh
@@ -11,38 +11,4 @@ DIRTY=""
 git status | grep -qF 'working directory clean' || DIRTY='-dirty'
 echo "<a href='http://github.com/timof/unisolar/commits/$COMMIT_FULL'>$BRANCH-$COMMIT$DIRTY</a>" >version.txt
 
-chmod 700 monitoring
-chmod 600 monitoring/exp
-chmod 700 monitoring/csv.download
-chmod 700 monitoring/xml.download
-chmod 700 monitoring/fix
-chmod 600 monitoring/FTPD.tmp
-chmod 700 monitoring/json.download
-chmod 600 monitoring/channels
-chmod 600 monitoring/out
-chmod 600 monitoring/FTPD.tmp.old
-chmod 600 monitoring/download.log
-chmod 700 monitoring/j
-chmod 600 monitoring/passwd
-chmod 600 monitoring/sample
-chmod 700 monitoring/zip
-chmod 600 monitoring/zip/2011-06-20_131000.xml
-chmod 600 monitoring/zip/2011-06-20_130000.xml
-chmod 600 monitoring/zip/2011-06-20_130500.xml
-chmod 600 monitoring/zip/2011-06-20_130000.zip
-chmod 755 daten
-chmod 644 daten/roof.php
-chmod 644 daten/d2.php
-chmod 644 daten/current.php
-chmod 600 daten/c
-chmod 600 daten/1current.php
-chmod 644 daten/c2.php
-chmod 644 daten/gray.png
-chmod 644 daten/last_production
-chmod 644 daten/lens.php
-chmod 777 daten/yellow.png
-chmod 777 daten/last
-chmod 644 daten/d.php
-chmod 644 daten/grandtotal
-chmod 777 daten/y.png
 chmod 700 .git
